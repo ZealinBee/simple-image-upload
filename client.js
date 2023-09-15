4145187be5738b8ee1ee3ec179d7ce0c8fe11540
Zhiyuan Liu <thenukezealot@gmail.com>
2023-09-16 00:48:30 +0300

    initial setup

---
+++ client.js
@@ -0,0 +1,43 @@
+const imagesContainer = document.querySelector(".images");
+
+window.addEventListener("load", fetchImages);
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const formData = new FormData();
+  formData.append("myFile", document.querySelector("#myFile").files[0]);
+  console.log(formData);
+  try {
+    const response = await fetch("http://localhost:3000/upload", {
+      method: "POST",
+      body: formData,
+    });
+
+    if (response.status === 200) {
+      alert("File uploaded successfully!");
+      fetchImages();
+    } else {
+      alert("File upload failed.");
+    }
+  } catch (error) {
+    console.error("Error uploading file:", error);
+  }
+});
+
+async function fetchImages() {
+  try {
+    const response = await fetch("http://localhost:3000/images");
+    const images = await response.json();
+    console.log(images)
+    let index = 0;
+    images.forEach((image) => {
+      const img = document.createElement("img");
+      img.src = `${images[index]}`;
+      imagesContainer.appendChild(img);
+      index++;
+    });
+  } catch {
+    console.error("Error fetching images");
+  }
+}
---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+
+const multer = require("multer");
+const app = express();
+app.use(cors());
+const fs = require("fs");
+
+const upload = multer({ dest: "uploads/" });
+const azureStorage = require("azure-storage");
+const { BlobServiceClient } = require("@azure/storage-blob");
+const blobService = azureStorage.createBlobService(
+  "DefaultEndpointsProtocol=https;AccountName=zealinbee;AccountKey=jrplFdWzhQpmWMr1hGyWLz3hj6Y1JB2nT8crAwkb3EW0IAHj4cYIS+9TQk/xtU120EXuvKeIJT9S+AStKBy3NA==;EndpointSuffix=core.windows.net"
+);
+
+app.post("/upload", upload.single("myFile"), function (req, res, next) {
+  console.log(req.file);
+  // Get file name and create read stream
+  const blobName = req.file.originalname;
+  const stream = fs.createReadStream(req.file.path);
+
+  // Get stream length
+  const streamLength = req.file.size;
+
+  // Upload file to Azure Blob Storage
+  blobService.createBlockBlobFromStream(
+    "images",
+    blobName,
+    stream,
+    streamLength,
+    (err) => {
+      if (err) {
+        // Handle error
+        console.error(err);
+        res.status(500).send(err);
+      } else {
+        // If no error, respond with success
+        res.status(200).send("File uploaded to Azure Blob Storage.");
+      }
+    }
+  );
+});
+
+app.get("/images", async (req, res) => {
+    try {
+        const images = []
+        blobService.listBlobsSegmented("images", null, (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                data.entries.forEach((blob) => {
+                    const url = blobService.getUrl("images", blob.name);
+                    images.push(url)
+                });
+                res.json(images);
+            }
+        })
+    }catch {
+        console.error("Error fetching images");
+    }
+});
+
+const corsOptions = {
+  origin: "http://127.0.0.1:5500",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
+app.listen(3000, () => console.log("Server listening on port 3000."));
